bb1d11740d14860765d5755d5be929f570bbde25
GraceeC <cardenasg85@yahoo.com>
2020-08-02 16:01:15 -0700

    snake & apple added

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
+// //text inside canvas for wins & apples eaten
+// function canvasText(){
+// let canvas =document.getElementById('myCanvas')
+// let text = canvas.getContext('2d');
 
-function canvasText(){
-let canvasDisplay =document.getElementById('myCanvas')
-let text = canvasDisplay.getContext('2d');
-text.font = "12px Georgia";
-text.fillStyle ='red';
-text.fillText('Apples Ate:', 200, 15);
-text.fillText('Wins:', 5, 15);
+// text.font = "12px Georgia";
+// text.fillStyle ='red';
 
+// text.fillText('Apples Ate:', 50, 15);
+// text.fillText('Wins:', 5, 15);
+
+// }
+// canvasText();
+//snake function which is the black sqaure
+function snake() {
+    let canvas = document.getElementById('my-canvas')
+    let snake = canvas.getContext('2d');
+
+    snake.beginPath();
+    snake.rect(10, 5, 5, 5);
+    snake.fill();
 }
-canvasText();
+snake();
 
+function apple() {
+    let canvas = document.getElementById('my-canvas')
+    let apple = canvas.getContext('2d');
+
+    apple.beginPath();
+    apple.rect(30, 10, 5, 5);
+    apple.fillStyle ='red';
+    apple.fill();
+    
+}
+apple();
